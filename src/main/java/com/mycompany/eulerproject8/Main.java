9515a11e9ed4904ef0058b43cc78fe106586032e
johnfanara <johnfanara1138@gmail.com>
2023-03-24 22:51:26 -0400

    creates int array of int arrays

--- src/main/java/com/mycompany/eulerproject8/Main.java
+++ src/main/java/com/mycompany/eulerproject8/Main.java
@@ -9,19 +9,22 @@ public class Main {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 
             9, 10, 11, 12, 13, 14, 15, 16};
                
-        int[][] bigArray = new int[array.length/4][4];    
+        int[][] bigArray = new int[array.length/4][];
         
-        for (int i=0; i<array.length; i++){
-           int k = i;
-           for (int j=0; j<4; j++){
-               bigArray[i][j] = array[k];
-               k++;
-           }
-           i+=3;     
-        } 
+        for (int i=0; i<bigArray.length; i++){
+            bigArray[i] = new int[4];
+        }
         
-        for (int i = 0; i < bigArray.length; i++){
-            for (int j = 0; j < 4; j++){
+        int k = 0;
+        for (int i=0; i<bigArray.length; i++){
+            for (int j=0; j<bigArray[i].length; j++){
+                bigArray[i][j] = array[k];
+                k++;
+            }
+        }
+        
+        for (int i=0; i<bigArray.length; i++) {
+            for(int j=0; j<bigArray[i].length; j++) {
                 System.out.print(bigArray[i][j] + " ");
             }
             System.out.println();
